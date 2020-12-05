3a13648c37cecc2e748e7d1e72ad82dfcbcc5b32
Tomas Ignacio Garcia <15tommgarcia@gmail.com>
2020-12-05 18:15:32 -0300

    Pruebo el comportamiento y los cambios de estado del tamagotchi

--- tamagotchi.py
+++ tamagotchi.py
@@ -8,7 +8,7 @@ class Tamagotchi:
         self.hambre=hambre
         self.energia=energia
 
-    def __jugar__(self):
+    def jugar(self):
         if self.energia<=0:
             print("Game Over")
         else:
@@ -16,7 +16,7 @@ class Tamagotchi:
             self.animo+=1
             self.energia-=1
 
-    def __alimentar__(self):
+    def alimentar(self):
         if self.energia<=0:
             print("Game Over")
         else:
@@ -24,7 +24,7 @@ class Tamagotchi:
             self.energia+=1
             self.hambre-=1
 
-    def __dormir__(self):
+    def dormir(self):
         if self.energia<=0:
             print("Game Over")
         else:
@@ -32,7 +32,7 @@ class Tamagotchi:
             self.energia+=1
             self.hambre+=1
 
-    def __pasar__(self):
+    def pasar(self):
         if self.energia<=0:
             print("Game Over")
         else:
@@ -41,5 +41,24 @@ class Tamagotchi:
             self.hambre+=1
             self.animo-=1
 
+    def __str__(self):
+        return ("Animo: %d Hambre: %d Energia: %d" %(self.animo,self.hambre,self.energia))
+
+    def mostrarEstado(self):
+        print ("Animo: %d Hambre: %d Energia: %d" %(self.animo,self.hambre,self.energia))
+
 t1 = Tamagotchi(20,0,82)
 t2 = Tamagotchi(25,10,60)
+
+
+print(t1)
+# t1.mostrarEstado()
+# ambos hacen lo mismo, mostar el estado del tamagotchi
+
+t1.jugar()
+print(t1)
+t1.pasar()
+t1.pasar()
+t1.dormir()
+t1.pasar()
+print(t1)
